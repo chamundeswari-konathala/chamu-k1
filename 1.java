b93ea78e32059ea8b3ce30bbd11c8b7d057074ac
chamundeswari-konathala <60577044+chamundeswari-konathala@users.noreply.github.com>
2020-02-08 12:15:35 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,18 @@
+
+ public class classA 
+{
+	public void display()
+	 {
+		 System.out.println("hai");
+		 
+	 }
+
+ public static void main(String args[])
+ {
+	 classA a=new classA();
+	 a.display();
+ }
+}
+ 
+	
+
